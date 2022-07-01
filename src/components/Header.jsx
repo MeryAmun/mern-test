a5adda2871ab3ce2535c8d4903f01d34dff24e7e
MeryAmun <chebesiawah@gmail.com>
2022-07-01 12:20:24 +0100

    initialized react boiler plate and created header

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header class="header">
+    <nav>
+        <div class="nav">
+            <div class="container">
+                <div class="nav__image">
+                    <h1>React-Test</h1>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
+  )
+}
+
+export default Header
